f73e74e1fbaaacc51148cae5a7851b0d600e5b8f
oliveir4Matheus <contato.matheusoliv@outlook.com>
2026-09-08 00:00:05 -0300

    Employees Whose Manager Left the Company: Time 388 ms (44.5%), Memory 0B (100.0%)

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT
+    e.employee_id
+FROM
+    Employees AS e
+LEFT JOIN
+    Employees AS m
+ON
+    e.manager_id = m.employee_id
+WHERE
+    e.salary < 30000
+AND
+    m.employee_id IS NULL
+AND
+    e.manager_id IS NOT NULL
+ORDER BY
+    e.employee_id
+
+    
